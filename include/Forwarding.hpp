62b5e8a396531e05d4ab52398d4a9882cfd8fa8c
Christian Janke <herr.christian.janke@web.de>
2020-01-01 20:33:03 +0100

    add signature_size meta function

--- include/Forwarding.hpp
+++ include/Forwarding.hpp
@@ -18,3 +18,18 @@ struct signature{};
 
 template<typename... IndexedOverload>
 struct indexed_overload_sequence{};
+
+//================================================================================
+//                          "std::tuple_size" for signature
+//================================================================================
+
+template<typename T>
+struct signature_size;
+
+template<typename... Types>
+struct signature_size<signature<Types...>>{
+    static constexpr size_t value = sizeof...(Types);
+};
+
+template<typename T>
+static constexpr size_t signature_size_v = signature_size<T>::value;
